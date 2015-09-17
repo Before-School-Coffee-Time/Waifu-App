4c1ec3ca5e7fa16caa45f8a2cff46f8b6d4f8f3e
zachser <zach.hoyler@gmail.com>
2015-09-16 23:16:16 -0400

    Made the textView clickable as a test and learning thing

--- app/src/main/java/bsct/waifu_app/MainActivity.java
+++ app/src/main/java/bsct/waifu_app/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void testButton(View v)
+    {
+        TextView text = (TextView) findViewById(R.id.textbox1);
+        text.setText("test part 2");
     }
 }
